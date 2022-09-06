61eea315109f9a659bcc4211c8974fb25b84e622
ElenA <vselova1201@yandx.ru>
2022-09-06 18:55:40 +0700

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,122 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int ONEOFCLASSHUMAN = 30;
+        int a = 50;
+        System.out.println("Значение переменной a c типом int равно " + a);
+        byte b = -5;
+        System.out.println("Значение переменной b c типом byte равно " + b);
+        short c = 32;
+        System.out.println("Значение переменной c c типом short равно " + c);
+        long d = 5000000000000000l;
+        System.out.println("Значение переменной d c типом long равно " + d);
+        double e = 3.3;
+        System.out.println("Значение переменной e c типом double равно " + e);
+        float v = 3.4f;
+        System.out.println("Значение переменной v c типом float равно " + v);
+        //Задание 2
+        double doubleValue = 27.12;
+        long intValue = 987678965549l;
+        float floatValue = 2.786f;
+        boolean booleanValue = false;
+        int dog = 569;
+        int dog2 = -159;
+        short dog3 = 27897;
+        byte djg4 = 67;
+        //Задание 3
+        int numberOfPupilsInClass1 = 23;
+        int numberOfPupilsInClass2 = 27;
+        int numberOfPupilsInClass3 = 30;
+        int totalAmountOfPupils = numberOfPupilsInClass1 + numberOfPupilsInClass2 + numberOfPupilsInClass3;
+        int numberOfPiecesOfPaperPerPupil = 480 / totalAmountOfPupils;
+        System.out.println("На каждого ученика рассчитано " + numberOfPiecesOfPaperPerPupil + " листов бумаги");
+        //Задание 4
+        int carOFbottles = 16;
+        int minut = 2;
+        int machinOFPerformanceminut = 20;
+        int days = 24;
+        int day = 3;
+        int moth = 31;
+        int durationOFoperationOFThemachine = carOFbottles / minut + carOFbottles / minut;
+        int durationOFoperationoOFThemachine = carOFbottles / days;
+        int durationOFroperationOFThemachine = carOFbottles / day + carOFbottles / moth;
+        System.out.println
+                ("За две минуты  продолжительность   работы машины carOFBottles /minut машины произвела бутылок 8 штук ");
+        System.out.println
+                ("За три дня  продолжительность   работы машины carOFBottles /days произвела бутылок 6 штук )");
+        System.out.println
+                ("За  месяц  продолжительность   работы машины carOFBottles / day произвела бутылок 2 штук)");
+        System.out.println
+                ("За двадцать минут  продолжительность   работы машины carOFBottles/minut  произвела бутылок 2 штук )");
+        System.out.println
+                ("За  сутки  продолжительность   работы машины carOFBottles /days произвела бутылок 2 штук )");
+        //Задание 5
+        int forrepairsshcooBank = 120;
+        int canOFWhitepaintforoneclass = 2;
+        int canOFPaintBrownforoneclass = 4;
+        int OneOFClassHuman = 30;
+        int humanclassOFone = 20;
+        int cansOFPaintPurchased = forrepairsshcooBank / canOFWhitepaintforoneclass - ONEOFCLASSHUMAN / 30;
+        int PurchasedPaintcansOF = forrepairsshcooBank / canOFPaintBrownforoneclass - humanclassOFone / 20;
+        System.out.println
+                ("В школе,где ONEOFCLASSHUMAN /30 нужно 50 банок белой краски и 1 банок коричневой краски");
+        System.out.println
+                ("В школе,где humanСlassOFOne /20 нужно 80 банок белой краски и 50 банок коричневой краски");
+        // задание 6
+        int bananasOFPieces = 5;
+        int oneOFBananaGrams = 80;
+        int milkOFMl = 200;
+        int millOFGramm = 100;
+        int iceOFcrammOFSundaeBriquettes = 2;
+        int gramsOFPerang = 100;
+        int rawOFEggs = 4;
+        int oneOFGramSegg = 70;
+        int weiGht = 5 * 80 + 200 + 2 * 100 + 4 * 100;
+        long weiGhtOFAllProducts = 602400l;
+        int weighOFAllProducts = 6024;
+        System.out.println("Kilogram 602400");
+        System.out.println("Gram 6204 ");
+        //задание 7
+        int weightOFGram = 7000;
+        int gramOFWeight = 250;
+        int grammOFWeight = 50;
+        char inDays = 2;
+        int grammOFWeinght = 7000 / 250;
+        int weinghtOFGram = 7000 / 500;
+        long gramOFWeinght = 14 + 28;
+        char daysIn = 42 / 2;
+        System.out.println("Moжет 21 потребоваться  дней в среднем чтобы добиться результата похудения");
+        //задание 8
+        double mashaReceivesRublesOFMonth1 = 67.760;
+        double denisReceivesRubleOFMonth2 = 83.690;
+        double kristinaReceivesRublesOFMonth2 = 76.230;
+        int salaryOFProcents = 10;
+        double salaryOFMacha = mashaReceivesRublesOFMonth1 + salaryOFProcents;
+        double salaryOFDenism = denisReceivesRubleOFMonth2 + salaryOFProcents;
+        double salaryOFkristina = kristinaReceivesRublesOFMonth2 + salaryOFProcents;
+        System.out.println
+                ("разница между годовым доходом с нынешней зарплатой mashaReceivesRublesOFMonth1+salaryOFProcents+и после повыщения");
+        System.out.println
+                ("разница между годовым доходом и нышней зарплатой denisReceivesRubleOFMonth2 + salaryOFProcents и после повыщения");
+        System.out.println
+                ("разница между годовым доходом и нынешней зарплатой" + kristinaReceivesRublesOFMonth2 + salaryOFProcents);
+        System.out.println("разница между годовым доходом и нынешней зарплатой+kristinaReceivesRublesOFMonth2+salaryOFProcents");
+        System.out.println("и после повышения");
+        System.out.println("разница между годовым доходом и нынешней зарплатой и после повышения mashaReceivesRublesOFMonth1+salaryOFProcents");
+        System.out.println("разница между годовым доходоми  и нынешней зарплатой и после повышения denisReceivesRubleOFMonth2 + salaryOFProcents ");
+        System.out.println("разница между годовым доходоми  и нынешней зарплатой и после повышения kristinaReceivesRublesOFMonth2+ salaryOFProcents");
+        System.out.println("разница в доходе до повышения mashaReceivesRublesOFMonth1-salaryOFProcents");
+        System.out.println("разница в доходе до повышения denisReceivesRubleOFMonth2-salaryOFProcents");
+        System.out.println("разница в доходе до повышения kristinaReceivesRublesOFMonth2-salaryOFProcents");
+        System.out.println("Маша теперь получаетmashaReceivesRublesOFMonth1+salaryOFProcents");
+        double annualOFIncome = mashaReceivesRublesOFMonth1 * 365;
+        double incomeOFAnnual = denisReceivesRubleOFMonth2 * 365;
+        double incomeOFAnnuale = kristinaReceivesRublesOFMonth2 * 365;
+        System.out.println("Маша теперь получает mashaReceivesRublesOFMonth1+salaryOFProcents рублей. Годовой доход вырос на mashaReceivesRublesOFMonth1 * 365рублей рублей ");
+        System.out.println("Денис теперь получает denisReceivesRubleOFMonth2 + salaryOFProcents рублей.Годовой доход вырос на denisReceivesRubleOFMonth2 * 365 рублей ");
+        System.out.println("Кристина теперь получает kristinaReceivesRublesOFMonth2+ salaryOFProcents рублей.Годовой вырос на kristinaReceivesRublesOFMonth2 * 365 рублей");
+
+
+
     }
 }
